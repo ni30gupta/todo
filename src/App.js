dc13b2daead88473eec4e411b46fdf5642045e75
ni30gupta <nguptani30@gmail.com>
2021-09-26 03:08:07 +0530

    test done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,108 @@
-import React, { useState } from 'react'
-// import useState from 'react'
+import React, { useState } from "react";
+
+let fetchData = () => {
+  let task_list = localStorage.getItem('task_list')
+  if (task_list) {
+    return JSON.parse(task_list)
+  } else {
+    return []
+  }
+}
 
 
 
 function App() {
-  const [task, setTask] = useState([])
-  const [input, setInput] = useState("")
-
-
+  const [task, setTask] = useState(fetchData);
+  const [input, setInput] = useState("");
+  const [isEdit, setisEdit] = useState(false);
+  const [editId, setEditId] = useState(null);
 
   const handleChange = (e) => {
-    setInput(e.target.value)
-
-  }
+    setInput(e.target.value);
+  };
   const handleKey = (e) => {
-    if (e.key === 'Enter') {
-      submitTask()
+    if (e.key === "Enter") {
+      submitTask();
     }
-  }
+  };
 
   const submitTask = () => {
-    if (input != "") {
-      setTask([...task, input])
+    if (input === "") {
+      alert("enter data");
+    } else if (input && isEdit) {
+      setTask(
+        task.map((item) => {
+          if (item.id === editId) {
+            return { ...item, name: input };
+          }
+          return item;
+        })
+      );
+      setInput("");
+      setisEdit(false);
+      setEditId(null);
+    } else {
+      let id = new Date().getTime().toString();
+      let actualTask = { id: id, name: input };
+      setTask([...task, actualTask]);
     }
+    setInput("");
+    console.log(task);
+    localStorage.setItem('data', JSON.stringify(task))
+  };
 
-    setInput("")
-  }
+  const deleteTask = (id) => {
+    setTask(
+      task.filter((data) => {
+        return data.id !== id;
+      })
+    );
+  };
+
+  const editTask = (id) => {
+    let editable = task.find((item) => {
+      return item.id === id;
+    });
+    // console.log(editable);
+    setInput(editable.name);
+    setisEdit(true);
+    setEditId(id);
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem('task_list', JSON.stringify(task))
+  }, [task])
 
   return (
     <div>
-      <input placeholder="Enter Task" type="text" onKeyPress={handleKey} value={input} onChange={handleChange} />
-      <button onClick={submitTask} >Add to List</button>
+      {/* {console.log(task)} */}
+      <input
+        autoFocus
+        placeholder="Enter Task"
+        type="text"
+        onKeyPress={handleKey}
+        value={input}
+        onChange={handleChange}
+      />
+      <button onClick={submitTask}>Add to List</button>
       <br />
-      {
-        task.map(data => {
-          return (<h4>{data} <span><button>Edit</button></span> <span><button>Delete</button></span>  </h4>)
-        })
-      }
+      <ul>
+        {task.map((data) => {
+          return (
+            <li id={`li${data.id}`} key={data.id}>
+              {data.name}{" "}
+              <span>
+                <button onClick={() => editTask(data.id)}>Edit</button>
+              </span>{" "}
+              <span>
+                <button onClick={() => deleteTask(data.id)}>Delete</button>
+              </span>{" "}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/FetchApp.js
@@ -0,0 +1,71 @@
+import { Button, ListGroup, Pagination } from 'react-bootstrap'
+import React from 'react'
+import './fetchApp.css'
+function FetchApp() {
+     const [data, setData] = React.useState({})
+     const [users, setUsers] = React.useState([])
+     const [pages, setPages] = React.useState([])
+     const [page_no, setPage_no] = React.useState(1)
+
+
+     React.useEffect(() => {
+          fetch(`https://reqres.in/api/users?page=${page_no}`)
+               .then(response => response.json())
+               .then(result => {
+                    setData(result)
+                    // setPage(result.total)
+                    setUsers(result.data)
+                    let init = []
+                    for (let i = 1; i < result.total + 1; i++) {
+                         init.push(i)
+                    }
+                    setPages(init)
+               })
+     }, [page_no])
+
+
+
+     const changePage = (page_id) => {
+          setPage_no(page_id)
+     }
+
+
+
+     let active = page_no;
+     let items = [];
+     for (let number = 1; number <= pages.length; number++) {
+          items.push(
+               <Pagination.Item onClick={() => changePage(number)} key={number} active={number === active}>
+                    {number}
+               </Pagination.Item>,
+          );
+     }
+     console.log(pages)
+     return (
+          <div className="main">
+               <div className="users">
+
+
+                    {
+                         users.length > 0 ? (users.map(user => {
+                              return (
+                                   <ListGroup>
+                                        <ListGroup.Item >{user.first_name}</ListGroup.Item>
+
+                                   </ListGroup>
+                              )
+                         })) : (<h1>No Data Found</h1>)
+                    }
+
+               </div>
+               <div className="page">
+                    <Pagination>{items}</Pagination>
+               </div>
+          </div >
+     )
+}
+
+
+
+
+export default FetchApp
